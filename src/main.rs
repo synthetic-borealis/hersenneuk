432ee28f2d7f8c39db27f9bf6a36c8e65e129c68
synthetic-borealis <snip.coloured@gmail.com>
2022-09-29 11:58:34 +0300

    feat: allow providing source file through command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use std::fs;
-use hersenneuk::*;
+use std::{fs, process, env};
+use hersenneuk::{run, clean_code, strip_comments, is_valid_brainfuck};
 
 fn main() {
-    // let source_file_path = "hello_world.bf";
-    let source_file_path = "hello_with_comments.bf";
-    let mut hello_world_code = fs::read_to_string(source_file_path)
+    let mut args: Vec<String> = env::args().collect();
+    let exe_name = args.remove(0).split("\\").last().unwrap().to_string();
+    if args.len() == 0 {
+        print_usage(exe_name);
+        println!("No input provided!");
+        process::exit(-1);
+    }
+    if args.contains(&"help".to_string()) || args.contains(&"--help".to_string()) {
+        print_usage(exe_name);
+        process::exit(0);
+    }
+    let source_file = args.remove(0);
+    let mut source_code = fs::read_to_string(source_file)
         .expect("Could not read input file!");
-    hello_world_code = clean_code(&strip_comments(&hello_world_code));
+    source_code = clean_code(&strip_comments(&source_code));
 
-    if is_valid_brainfuck(&hello_world_code) {
-        run(&hello_world_code);
+    if is_valid_brainfuck(&source_code) {
+        run(&source_code);
     } else {
         println!("Oh noes :'(");
+        process::exit(-1);
     }
+}
+
+fn print_usage(exe_name: String) {
+    println!("Hersenneuk v0.0.1");
+    println!("Usage: {} SOURCE-FILE", exe_name);
 }
